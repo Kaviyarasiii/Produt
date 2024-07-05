5caca3f6cebbecccc2685903fe4335ce94ba994b
Kaviyarasiii <136569304+Kaviyarasiii@users.noreply.github.com>
2024-07-05 09:53:01 +0530

    Update ProductService.java

--- Product/src/main/java/com/example/Product/Service/ProductService.java
+++ Product/src/main/java/com/example/Product/Service/ProductService.java
@@ -5,31 +5,38 @@ import com.example.Product.model.Product;
 import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @Service
-public class ProductService {
-    @Autowired
-    private ProductRepository productRepository;
+@RequiredArgsConstructor
+public class ProductService { // Once the best practices are implemented through the interface.
 
-    public List<Product> listAll() {
-        return productRepository.findAll();
+    private final ProductRepository productRepository;
+
+    public Stream<Product> listAll() {
+        return productRepository.findAll().parallelStream().distinct();
     }
 
+   // return the saved entity object and give saved response
     public void save(Product product) {
         productRepository.save(product);
     }
 
     public Product get(Long id) {
-        return productRepository.findById(id).orElse(null);
+        return productRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("No entity found with id: " + id));
     }
 
     public void delete(Long id) {
+
+       productRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("No entity found with id: " + id));
+
         productRepository.deleteById(id);
     }
 }
-
 
