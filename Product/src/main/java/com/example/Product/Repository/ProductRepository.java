0cfdd548b4301c1188c86b9ac63b9b74da3ffff5
Kaviyarasiii <136569304+Kaviyarasiii@users.noreply.github.com>
2024-07-05 09:53:42 +0530

    Update ProductRepository.java

--- Product/src/main/java/com/example/Product/Repository/ProductRepository.java
+++ Product/src/main/java/com/example/Product/Repository/ProductRepository.java
@@ -3,5 +3,6 @@ package com.example.Product.Repository;
 import com.example.Product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }
